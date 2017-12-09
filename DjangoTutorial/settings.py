39533b093389fcc066e6f24fc0131c62d8480d42
MaybeGitHub <MaybePython@bla.com>
2017-12-09 10:35:37 +0000

    Settings

--- DjangoTutorial/settings.py
+++ DjangoTutorial/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ac3089fb-6b6d-45ac-a0dc-ed68870ac013'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
