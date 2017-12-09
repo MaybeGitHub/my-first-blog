ef8a34203481b6816ddd8920c59310685f92803a
Jesus <jesus.salvador.barcenilla@gmail.com>
2017-12-09 03:41:30 +0100

    Proyecto tutorial Finalizado

--- DjangoTutorial/urls.py
+++ DjangoTutorial/urls.py
@@ -2,18 +2,10 @@
 Definition of urls for DjangoTutorial.
 """
 
-from datetime import datetime
-from django.conf.urls import url, include
-import django.contrib.auth.views
-
-import app.forms
-import app.views
-
-# Uncomment the next lines to enable the admin:
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', include("blog.urls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
